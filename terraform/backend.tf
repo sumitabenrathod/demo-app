de51c2f1560745cb3760807049b89ef5d024269a
sumitabenrathod <sumitabenrathod087@gmail.com>
2026-01-05 12:05:02 +0530

    udpate tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -8,13 +8,3 @@ terraform {
   }
 }
 
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.30.0"
-    }
-  }
-}
