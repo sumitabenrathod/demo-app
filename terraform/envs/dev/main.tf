37bdee63df2471a5ae1182ca2b06174f7b2b7cc6
sumitabenrathod <sumitabenrathod087@gmail.com>
2026-01-05 11:46:58 +0530

    update

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -1,7 +1,21 @@
+module "vpc" {
+  source          = "../../modules/vpc"
+  env             = "dev"
+  cidr            = "10.0.0.0/16"
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
 module "eks" {
-  source       = "../../modules/eks"
-  cluster_name = "eks-dev"
-  env          = "dev"
+  source          = "../../modules/eks"
+  env             = "dev"
+  cluster_name    = "eks-dev"
   vpc_id          = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnets
+}
+
+module "ecr" {
+  source = "../../modules/ecr"
+  env    = "dev"
 }
