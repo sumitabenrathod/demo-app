b64834a213b1531c39ca4f316502a2502ea97d02
sumitabenrathod <sumitabenrathod087@gmail.com>
2026-01-05 11:42:49 +0530

    update tf

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -2,4 +2,6 @@ module "eks" {
   source       = "../../modules/eks"
   cluster_name = "eks-dev"
   env          = "dev"
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
 }
--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -2,4 +2,6 @@ module "eks" {
   source       = "../../modules/eks"
   cluster_name = "eks-prod"
   env          = "prod"
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets  
 }
